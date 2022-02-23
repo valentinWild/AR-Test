3d3442cc47e43bbc7c91c9e218f4c2f224504d2b
valentinWild <74301124+valentinWild@users.noreply.github.com>
2022-02-23 20:45:27 +0100

    "fix: deleted light sensor if-clause"

--- scripts/main.js
+++ scripts/main.js
@@ -1,15 +1,13 @@
 var lightLevel = "";
 
-if ('AmbientLightSensor' in window) {
-    const sensor = new AmbientLightSensor();
-    sensor.addEventListener('reading', event => {
-        console.log('CurrentLightLevel: ', sensor.illuminance);
-        lightLevel = sensor.illuminance;
-        document.querySelector("#lightlevel").innerHTML = lightLevel;
-    });
-    sensor.addEventListener('error', event => {
-        console.log(event.error.name, event.error.message);
-    });
+const sensor = new AmbientLightSensor();
+sensor.addEventListener('reading', event => {
+    console.log('CurrentLightLevel: ', sensor.illuminance);
+    lightLevel = sensor.illuminance;
+    document.querySelector("#lightlevel").innerHTML = lightLevel;
+});
+sensor.addEventListener('error', event => {
+    console.log(event.error.name, event.error.message);
+});
 
-    sensor.start();
-}
+sensor.start();
